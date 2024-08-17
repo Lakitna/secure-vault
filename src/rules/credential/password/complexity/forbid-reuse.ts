cf17c9898d577d523f2839def4d33fb7ce0a2f23
Sander van Beek <lakitna@gmail.com>
2024-08-17 11:15:37 +0200

    Moved security checker rules

---
+++ src/rules/credential/password/complexity/forbid-reuse.ts
@@ -0,0 +1,66 @@
+import { Rule } from 'rulebound';
+import { Credential } from '../../../../credentials';
+import { CredentialRuleParameters } from '../../../../vault/enforcable';
+import type { ReadableVault } from '../../../../vault/readable';
+
+export function credentialPasswordComplexityForbidReuse() {
+    return new Rule<CredentialRuleParameters>('credential/password/complexity/forbid-reuse')
+        .describe(
+            `
+            Ensure that the credential password is not used by another credential in the vault.
+
+            Reusing the same password in different places puts you at a higher risk of having your
+            accounts compromised. If a hacker is able to obtain your password, they will be able to
+            access all of the accounts that use that password. Using a different password for each
+            account protects from this type of attack.
+            `
+        )
+        .enable(async ({ config, credential }) => {
+            const forbidReuse = config.credentialRestrictions.passwordComplexity.forbidReuse;
+            if (forbidReuse === false) {
+                return 'Disabled by security config `forbidReuse`';
+            }
+
+            const thisPassword = credential.data.password;
+            if (thisPassword.length === 0) {
+                return 'No password, nothing to check';
+            }
+
+            return true;
+        })
+        .define(async ({ vault, credential }) => {
+            const thisPassword = credential.data.password;
+
+            const otherCredentials = await getOtherCredentials(vault, credential);
+
+            for (const otherCredential of otherCredentials) {
+                const otherPassword = otherCredential.data.password;
+
+                if (!thisPassword.equals(otherPassword)) {
+                    // Not the same password.
+                    continue;
+                }
+
+                throw new Error(
+                    `Credential password is used by another credential: ` +
+                        `'${otherCredential.path.join('/')}'`
+                );
+            }
+
+            return true;
+        });
+}
+
+async function getOtherCredentials(vault: ReadableVault, credential: Credential) {
+    const withoutSecrets = await vault.listCredentials();
+
+    const otherCredentials = await Promise.all(
+        withoutSecrets
+            .filter((other) => other.id !== credential.id)
+            .map(({ id }) => {
+                return vault.getCredentialById(id, { secure: false });
+            })
+    );
+
+    return otherCredentials.filter((cred): cred is Credential => cred !== null);
+}
---
+++ src/rules/vault/password/age.ts
@@ -0,0 +1,62 @@
+import { Rule } from 'rulebound';
+import { VaultRuleParameters } from '../../../vault/enforcable';
+
+export function vaultPasswordAge() {
+    return new Rule<VaultRuleParameters>('vault/password/age')
+        .describe(
+            `
+            Ensure that your vault password is not used for too long.
+
+            Over time, more and more people will know your password. Periodically changing it will
+            ensure that only the right people will be able to access the vault.
+
+            It's common for teams to change members. Ideally, you would change the vault password
+            every time someone leaves the team, but we tend to forget such things. Periodically
+            changing the password is a mitigation tactic to reduce the risk of old team members
+            having access to things they are not allowed to see.
+
+            We humans are famously bad at assessing risks, causing us to accidentally expose secrets
+            like the vault password. If you use the vault password for a long time, the risk of it
+            being exposed somewhere increases. Periodically changing the password is a mitigation
+            tactic to reduce the risk of (accidental) password exposure.
+            `
+        )
+        .enable(({ config }) => {
+            const maxPasswordAge = config.vaultRestrictions.maxPasswordAge;
+            if (maxPasswordAge <= 0) {
+                throw new TypeError(
+                    'Configuration error: Max password age can not be equal to or below 0'
+                );
+            }
+
+            if (maxPasswordAge === Infinity) {
+                // Will always be true, no need to compute
+                return 'Disabled by security config `maxPasswordAge`';
+            }
+
+            return true;
+        })
+        .define(async ({ config, vault }) => {
+            const maxPasswordAge = config.vaultRestrictions.maxPasswordAge;
+
+            const passwordChanged = await vault.getVaultPasswordLastChangeDate();
+            if (!passwordChanged) {
+                throw new Error(
+                    'Could not find when the vault password was last changed. Assuming the worst.'
+                );
+            }
+
+            const passwordAge = toAge(passwordChanged);
+            if (passwordAge > maxPasswordAge) {
+                throw new Error(`Vault password is too old, change it`);
+            }
+
+            return true;
+        });
+}
+
+function toAge(time: Date): number {
+    const now = new Date();
+    const hourInMilliseconds = 3600000;
+    return (now.getTime() - time.getTime()) / hourInMilliseconds;
+}
